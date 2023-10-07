0521b38a5e2e7fd9298aed7f3498f49f0373f982
Layzerg11 <vspishka17@mail.ru>
2023-10-07 12:56:54 +0400

    version#3

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.services.RoleServiceImp;
-import ru.kata.spring.boot_security.demo.services.UserServiceImp;
+import ru.kata.spring.boot_security.demo.services.RoleService;
+import ru.kata.spring.boot_security.demo.services.UserService;
 
 import java.security.Principal;
 
@@ -14,40 +14,40 @@ import java.security.Principal;
 @RequestMapping("/adminBootstrap")
 public class AdminController {
 
-    private final UserServiceImp userServiceImp;
+    private final UserService userService;
 
-    private final RoleServiceImp roleServiceImp;
+    private final RoleService roleService;
 
     @Autowired
-    public AdminController(UserServiceImp userServiceImp, RoleServiceImp roleServiceImp) {
-        this.userServiceImp = userServiceImp;
-        this.roleServiceImp = roleServiceImp;
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
     }
 
     @GetMapping
     public String pageForAdmin(Model model, Principal principal) {
-        model.addAttribute("admin", userServiceImp.findByUsername(principal.getName()));
-        model.addAttribute("users", userServiceImp.getUserList());
+        model.addAttribute("admin", userService.findByUsername(principal.getName()));
+        model.addAttribute("users", userService.getUserList());
         model.addAttribute("user", new User());
-        model.addAttribute("roles", roleServiceImp.findAllRoles());
+        model.addAttribute("roles", roleService.findAllRoles());
         return "adminBootstrap";
     }
 
     @PostMapping
     public String create(@ModelAttribute("user") User user) {
-        userServiceImp.createUser(user);
+        userService.createUser(user);
         return "redirect:/adminBootstrap";
     }
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("user") User user) {
-        userServiceImp.updateUser(user);
+        userService.updateUser(user);
         return "redirect:/adminBootstrap";
     }
 
     @DeleteMapping
     public String delete(@ModelAttribute("user") User user) {
-        userServiceImp.deleteUser(user.getId());
+        userService.deleteUser(user.getId());
         return "redirect:/adminBootstrap";
     }
 
--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -4,23 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.kata.spring.boot_security.demo.services.UserServiceImp;
+import ru.kata.spring.boot_security.demo.services.UserService;
 
 import java.security.Principal;
 
 @Controller
 public class UserController {
 
-    private final UserServiceImp userServiceImp;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserServiceImp userServiceImp) {
-        this.userServiceImp = userServiceImp;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/user")
     public String show(Principal principal, Model model) {
-        model.addAttribute("user", userServiceImp.findByUsername(principal.getName()));
+        model.addAttribute("user", userService.findByUsername(principal.getName()));
         return "userBootstrap";
     }
 
